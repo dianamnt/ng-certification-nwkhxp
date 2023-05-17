910cf0f2e1f9f7088ffd71f52c907d5b7c1bd9d9
dianamnt <45576054+dianamnt@users.noreply.github.com>
2023-05-17 21:38:18 +0300

    1 file modified, 3 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CurrentForecastPageComponent } from './components/current-forecast-page/current-forecast-page.component';
+import { FiveDayForecastPageComponent } from './components/five-day-forecast-page/five-day-forecast-page.component';
 
 const routes: Routes = [
   { path: 'forecast', component: CurrentForecastPageComponent },
+  { path: 'forecast/:id', component: FiveDayForecastPageComponent },
   { path: '', redirectTo: 'forecast', pathMatch: 'full' },
 ];
 
---
+++ src/app/components/five-day-forecast-page/five-day-forecast-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-five-day-forecast-page',
+  templateUrl: './five-day-forecast-page.component.html',
+  styleUrls: ['./five-day-forecast-page.component.css']
+})
+export class FiveDayForecastPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
